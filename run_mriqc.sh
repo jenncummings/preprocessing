a9767633d23025def6d9ea24521e90f49d8740ef
Cummings, Jenn <jenncummings@ucsf.edu>
2022-02-04 12:37:32 -0800

    change mriqc version

--- run_mriqc.sh
+++ run_mriqc.sh
@@ -9,7 +9,7 @@ subj=$1
 /usr/local/bin/singularity run --cleanenv \
     --bind $project_dir:/project \
     --bind $scratch_dir:/scratch \
-    /working/mochila2/JC/containers/mriqc.simg \
+    /working/mochila2/JC/containers/mriqc-v21.0.0.simg \
     --participant-label $subj \
     --correct-slice-timing --no-sub \
     --nprocs 8 -w /scratch \
--- run_mriqc_group.sh
+++ run_mriqc_group.sh
@@ -5,7 +5,7 @@ source globals.sh
 /usr/local/bin/singularity run --cleanenv \
     --bind $project_dir:/project \
     --bind $scratch_dir:/scratch \
-    /working/mochila2/JC/containers/mriqc.simg \
+    /working/mochila2/JC/containers/mriqc-v21.0.0.simg \
     --correct-slice-timing --modalities T1w bold \
     --no-sub \
     --nprocs 8 -w /scratch \
