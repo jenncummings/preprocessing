66cfacfdb24b17e94f0ad096a1adc1668fc68de4
Cummings, Jenn <jenncummings@ucsf.edu>
2022-07-06 12:15:43 -0700

    working version that runs w slurm

--- globals.sh
+++ globals.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#! /bin/bash
 
 # Edit where your project directory is
-export project_dir=/working/mochila2/JC
+project_dir=/working/mochila2/JC
 
-export data_dir=/data/mochila1/PatientStudy/UCSF
+data_dir=/data/mochila1/PatientStudy/UCSF
 
-export bids_dir=$project_dir/data/455_bids
+bids_dir=$project_dir/data/bids
 
-export scripts_dir=$project_dir/code/preprocessing #directory with my preprocessing scripts, including this one
+scripts_dir=$project_dir/code/preprocessing #directory with my preprocessing scripts, including this one
 
-export scratch_dir=/scratch/$USERNAME
+scratch_dir=/scratch/$USERNAME
 
-export work_dir=/working/mochila2/JC/work
+work_dir=/working/mochila2/JC/work
--- run_fmriprep.sh
+++ run_fmriprep.sh
@@ -7,18 +7,20 @@ source globals.sh
 subj=$1
 
 #Run fmriprep
-singularity run --cleanenv \
-    --bind $project_dir:/project \
-    --bind $work_dir:/scratch \
-    /working/mochila2/JC/containers/fmriprep-v21.0.1.simg \
-    --verbose \
+singularity run -e -B $project_dir:/project,$work_dir:/scratch /data/i2/software/singularity/fmriprep-latest.simg \
+    /project/data/new_testbids /project/data/new_testbids/derivatives \
+    participant \
     --participant-label $subj \
     --fs-license-file /project/code/preprocessing/license.txt \
+    --verbose \
+    --debug all \
     --fs-no-reconall \
     --omp-nthreads $SLURM_CPUS_PER_TASK \
     --n_cpus $SLURM_CPUS_PER_TASK \
     --use-aroma \
     --return-all-components \
+    --resource-monitor \
     --output-spaces MNI152NLin2009cAsym \
     --write-graph --work-dir /scratch \
-    /project/data/testbids_2 /project/data/testbids_2/derivatives participant
+
+echo "Completed fmriprep script for BACPAC sub-$subj."
