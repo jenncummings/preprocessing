c5839636b18ee37963efc987e4e156d88fb4b8b1
Cummings, Jenn <jenncummings@ucsf.edu>
2022-07-06 15:38:17 -0700

    scripts for running newer singularity container

---
+++ latest_run_fmriprep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Edited from Pygers workshop template
+
+project_dir=/working/mochila2/JC
+work_dir=/working/mochila2/JC/work_2
+
+#Begin:
+subj=$1
+
+#Run fmriprep
+singularity run -e -B $project_dir:/project,$work_dir:/scratch /working/mochila2/JC/containers/fmriprep-21.0.2.simg \
+    /project/data/latest_new_testbids /project/data/latest_new_testbids/derivatives \
+    participant \
+    --participant-label $subj \
+    --fs-license-file /project/code/preprocessing/license.txt \
+    --verbose \
+    --debug all \
+    --fs-no-reconall \
+    --omp-nthreads $SLURM_CPUS_PER_TASK \
+    --n_cpus $SLURM_CPUS_PER_TASK \
+    --use-aroma \
+    --return-all-components \
+    --resource-monitor \
+    --output-spaces MNI152NLin2009cAsym \
+    --write-graph --work-dir /scratch \
+
+echo "Completed fmriprep script for BACPAC sub-$subj."
