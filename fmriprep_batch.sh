f7bc4c4f12b3cae367fe56a5c28153a199d02375
Cummings, Jenn <jenncummings@ucsf.edu>
2022-07-15 15:23:26 -0700

    single file for slurm and fmriprep call

---
+++ fmriprep_batch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=nosdc_fmriprep_v21.0.2
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-user=jenncummings@ucsf.edu
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --time=48:00:00
+#SBATCH --output=/working/mochila2/JC/code/preprocessing/logs/%x-%j.log
+
+pwd; hostname; date
+
+echo "Running fmriprep script for BACPAC sub-${1}."
+
+#User inputs:
+project_dir=/working/mochila2/JC
+bids_dir=$project_dir/data/bids
+work_dir=$project_dir/work
+
+#Begin:
+
+subj=${1}
+
+#Run fmriprep
+
+singularity run -e -B $bids_dir:/BIDS,$work_dir:/work /working/mochila2/JC/containers/fmriprep-21.0.2.simg \
+    /BIDS /BIDS/derivatives \
+    participant \
+    --omp-nthreads $SLURM_CPUS_PER_TASK \
+    --nprocs $SLURM_CPUS_PER_TASK \
+    --participant-label $subj \
+    --ignore fieldmaps \
+    --bold2t1w-dof 9 \
+    --fs-no-reconall \
+    --use-aroma \
+    --debug all \
+    --resource-monitor \
+    --fs-license-file $project_dir/code/preprocessing/license.txt \
+    --output-spaces MNI152NLin2009cAsym \
+    -w /work \
+
+echo "Completed fmriprep script for BrainChange sub-bc${1}."
