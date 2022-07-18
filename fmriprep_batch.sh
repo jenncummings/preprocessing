553845880bc78bcd5178bc0093ba309ffdd00c87
Cummings, Jenn <jenncummings@ucsf.edu>
2022-07-18 14:46:33 -0700

    no freesurfer

--- fmriprep_batch.sh
+++ fmriprep_batch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=nosdc_fmriprep_v21.0.2
 #SBATCH --mail-type=START,END,FAIL
 #SBATCH --mail-user=jenncummings@ucsf.edu
 #SBATCH --nodes=1
@@ -17,7 +16,7 @@ echo "Running fmriprep script for BACPAC sub-${1}."
 #User inputs:
 project_dir=/working/mochila2/JC
 bids_dir=$project_dir/data/bids
-work_dir=$project_dir/work_2
+work_dir=$project_dir/work
 
 #Begin:
 subj=${1}
@@ -25,7 +24,7 @@ subj=${1}
 #Run fmriprep
 
 singularity run -e -B $bids_dir:/BIDS,$work_dir:/work /working/mochila2/JC/containers/fmriprep-21.0.2.simg \
-    /BIDS /BIDS/derivatives \
+    /BIDS /BIDS/derivatives/nofm \
     participant \
     --omp-nthreads $SLURM_CPUS_PER_TASK \
     --n_cpus $SLURM_CPUS_PER_TASK \
@@ -33,6 +32,7 @@ singularity run -e -B $bids_dir:/BIDS,$work_dir:/work /working/mochila2/JC/conta
     --ignore fieldmaps \
     --bold2t1w-dof 9 \
     --use-aroma \
+    --fs-no-reconall \
     --debug all \
     --resource-monitor \
     --fs-license-file $project_dir/code/preprocessing/license.txt \
